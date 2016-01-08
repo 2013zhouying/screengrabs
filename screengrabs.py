0fda058adc3a227d9532732606a12755fc4869ac
unknown <cron@NWPBPBP0DPC2334.cmg.int>
2016-01-08 10:47:05 -0500

    Fix for delay

--- screengrabs.py
+++ screengrabs.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from PIL import Image
 import os
-from time import strftime
+from time import strftime, sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -19,6 +19,8 @@ mysites = [
     ]
 
 
+delay = 45      # Time to wait to finish loading AJAX-y things after main page loads.   
+    
 
 datepath = strftime("/%Y/%m/%d/")
 filesuffix = strftime("_%Y-%m-%d_%H%M")
@@ -30,6 +32,8 @@ browser = webdriver.Firefox()
 for site in mysites:
     URL, abbreviation = site
     browser.get(URL)
+    sleep(delay)
+## Below didn't work, so above is a replacement.    
 #    delay = 20 # Maximum page load time for extras like AJAX
 #    try:
 #        WebDriverWait(browser, delay).until(EC.presence_of_element_located(browser.find_element_by_id('IdOfMyElement')))
@@ -40,9 +44,3 @@ for site in mysites:
     im = Image.open(masterdirectory + '/screenie.png').save(masterdirectory + datepath + abbreviation + filesuffix + ".jpg")
 
 browser.quit()
-
-#im = Image.open(masterdirectory + '/screenie.png').save(masterdirectory + 'screenie.jpg')
-#im = Image.open('screenie.png').save('screenie.jpg')
-
-# im.save('screenie8.jpg', quality=8)
-# 6mb/hr. 150mb/day. 
